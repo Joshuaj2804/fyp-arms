0908e09544bd375afde700eb2aead8a9a86d2b8b
joshua <jos007280499@gmail.com>
2019-10-06 01:05:15 +0800

    hello

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn backend.wsgi
+web: gunicorn Lab.wsgi
--- backend/Lab/Lab/settings.py
+++ backend/Lab/Lab/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,4 +146,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_ADAPTER = 'fyp.adapter.CustomAccountAdapter'
 
-django_heroku.settings(locals())
